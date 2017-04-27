d8e48c9a5ce45cee96b1ae9ac2fb6c1674d8bd25
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-04-27 17:32:11 -0400

    fix databases

---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+//app/controllers/index.js
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  isDisabled: true
+
+});
--- config/environment.js
+++ config/environment.js
@@ -1,50 +1,17 @@
-/* eslint-env node */
+// config/environment.js
 
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'library-app',
     environment: environment,
-    rootURL: '/',
+    baseURL: '/',
     locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+    firebase: {
+    apiKey: "AIzaSyChdk01nyB4e16vRokctqRwK8-0rInk8d0",
+    authDomain: "library-9f64d.firebaseapp.com",
+    databaseURL: "https://library-9f64d.firebaseio.com",
+    projectId: "library-9f64d",
+    storageBucket: "library-9f64d.appspot.com",
+    messagingSenderId: "124877017978"
     }
-  };
-
-  if (environment === 'development') {
-    ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-
-  }
-
-  return ENV;
-};
